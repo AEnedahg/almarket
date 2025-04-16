394814590c1ad38bb08b8191d8e63922f47f4cf6
Anthony Enedah <enedahanthony54@gmail.com>
2025-04-16 18:19:43 +0100

    removed footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Metrics from '@/components/Metrics';
 import Process from '@/components/Process';
 import CTA from '@/components/CTA';
 import Hero from '@/components/Hero';
-import Footer from '@/components/footer';
+
 export default function Home() {
   return (
     <div>
@@ -13,7 +13,6 @@ export default function Home() {
       <Metrics />
       <Process />
       <CTA />
-      <Footer />
     </div>    
   );
 }
